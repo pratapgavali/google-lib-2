48a46966ae3d4a3051dbfdec46fb744b5e1a77fb
ADMIN <USER@ADMINs-MacBook-Pro.local>
2022-04-17 23:12:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home/Home";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Book from "./Book/Book";
+import SearchBook from "./SearchBook/SearchBook";
+import UserSearchLogs from "./UserLogs/UserSearchLogs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookInfo" exact element={<SearchBook />} />
+        <Route path="/userLog" exact element={<UserSearchLogs />} />
+      </Routes>
+    </Router>
   );
 }
 
